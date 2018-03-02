717e2da5f92e943e2381ef269a6f6bc25a16e4a1
NipunaPrashan <nipunaprashan99@gmail.com>
2018-03-02 17:52:56 +0530

    Change paths in configure.sh

--- configure-gateway.sh
+++ configure-gateway.sh
@@ -6,14 +6,14 @@ echo "Downloading WSO2 On-Prem API Gateway..."
 wget $ON_PREM_GATEWAY_DOWNLOAD_LINK
 echo "Setting up WSO2 On-prem API Gateway..."
 unzip -q $DOWNLOAD_ZIP_FILE_NAME
-
+pwd
 #Move to On-premise root directory.
 #cd $UNZIPPED_FILE_NAME
 
 
 #Binding Heroku dynamic port to Axis2 synapse port.
-sed -i 's/ORG_KEY=""/ORG_KEY="${WSO2_CLOUD_ORG_KEY}"/' bin/configure-gateway.sh
-sed -i 's/EMAIL=""/EMAIL="${WSO2_CLOUD_EMAIL}"/' bin/configure-gateway.sh
-sed -i 's/PASSWORD=""/PASSWORD="${WSO2_CLOUD_PASSWORD}"/' bin/configure-gateway.sh 
-echo 'sed -i "s/8280/$PORT/" repository/conf/axis2/axis2.xml' >> bin/configure-gateway.sh
-echo 'bash bin/wso2server.sh -Dsetup' >> bin/configure-gateway.sh
+sed -i 's/ORG_KEY=""/ORG_KEY="${WSO2_CLOUD_ORG_KEY}"/' wso2am-2.1.0/bin/configure-gateway.sh
+sed -i 's/EMAIL=""/EMAIL="${WSO2_CLOUD_EMAIL}"/' wso2am-2.1.0/bin/configure-gateway.sh
+sed -i 's/PASSWORD=""/PASSWORD="${WSO2_CLOUD_PASSWORD}"/' wso2am-2.1.0/bin/configure-gateway.sh 
+echo 'sed -i "s/8280/$PORT/" repository/conf/axis2/axis2.xml' >> wso2am-2.1.0/bin/configure-gateway.sh
+echo 'bash bin/wso2server.sh -Dsetup' >> wso2am-2.1.0/bin/configure-gateway.sh
