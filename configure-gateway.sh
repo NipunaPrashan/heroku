57a7c95c77ea28020770fd294f31ed3209dc4b3d
NipunaPrashan <nipunaprashan99@gmail.com>
2018-03-05 14:20:49 +0530

    fixing issues with configure-gateway.sh

--- configure-gateway.sh
+++ configure-gateway.sh
@@ -28,11 +28,9 @@ if [ ! -f $DOWNLOAD_ZIP_FILE_NAME ]
 	    echo 'bash wso2am-2.1.0/bin/wso2server.sh' >> $UNZIPPED_FILE_NAME/bin/configure-gateway.sh
         fi
 fi
-sh $UNZIPPED_FILE_NAME/bin/configure-gateway.sh
-if [ -f $DOWNLOAD_ZIP_FILE_NAME ]; then
-    echo "###################deleting####################"
-    rm -rf DOWNLOAD_ZIP_FILE_NAME
-    
-fi
-
+ls
+echo "###################deleting####################"
+rm -rf DOWNLOAD_ZIP_FILE_NAME
 ls -lh
+
+sh $UNZIPPED_FILE_NAME/bin/configure-gateway.sh
